ceb6dc48b2e0ae04944de4cae02526648c36e9ce
Jakub Stachurski <jakub@wilkuu.xyz>
2023-10-20 23:30:13 +0200

    Comment code in PControllerPhys

--- Assets/Scripts/Movement/PControllerPhys.cs
+++ Assets/Scripts/Movement/PControllerPhys.cs
@@ -93,7 +93,8 @@ namespace Movement {
             // TODO implement speed check
             return rb.velocity.magnitude > o_walk.slideMinVel;
         }
-
+        
+        // This function is the entire state machine of the different kinds of movement, excluding input. 
         void determineMode() {
 #if false 
             Debug.Log(string.Format("\ngrounded: {0} | mode: {1} | velocity {2}",
@@ -101,61 +102,85 @@ namespace Movement {
                        state.mode.current,
                        rb.velocity.y));
 #endif
+
+           // Transitions when touching ground 
+           // EXCLUDES: 
+           //   Jump key: ANY -> JUMP 
+           //   Slide key: ANY -> SLIDE 
            if (state.isGrounded) {
+               // Transition out of jumping
                 if (state.mode == MovementMode.SLIDE)
-                    if (CanSlide())
-                        state.mode.Set(MovementMode.SLIDE);
-                    else {
+                    if (!CanSlide()){
                         state.mode.Set(MovementMode.WALK);
                         hb_anim.SetBool("isSliding",false);
                     }
-                else if (state.mode == MovementMode.JUMP && rb.velocity.y > -0.1)
-                    state.mode.Set(MovementMode.JUMP);
+                // Transition 
+                //else if (state.mode == MovementMode.JUMP && rb.velocity.y > -0.1)
+                //    state.mode.Set(MovementMode.JUMP);
+                
+                // Transition from NOT SLIDE OR WALK to WALK 
                 else if (state.mode != MovementMode.WALK)
                 {
                     state.mode.Set(MovementMode.WALK);
-                    state.curAirActions = 0;
+                    state.curAirActions = 0; // Reset jumping 
                 }
            } else {
+                // When falling and not sliding go to AIR 
                 if (!((state.mode == MovementMode.JUMP && rb.velocity.y > -0.1) ||
                      state.mode == MovementMode.SLIDE ||
                      state.mode == MovementMode.WALL))
                     state.mode.Set(MovementMode.AIR);
-
+                
+                // Set coyote time if fallen off a ledge while walking
+                // TODO: This should be moved somewhere where it fits more
+                // TODO: This introduced a bug where you don't have coyote time 
+                //        when sliding off a ledge
                 if (state.mode.previous == MovementMode.WALK && 
                     state.mode.current == MovementMode.AIR) 
                     o_air.coyoteTime.Set();
            }
         }
-
+        
+        // This method is the magic that keeps the hitbox above the grounds so one can use stairs
         void applyFloatation(){
+            // Check if we have ground below us 
+            // TODOD: isGrounded can be used for consistency
             if(detection[SRLegacy.DIR.DOWN])
             {
+                // Get the height at which we want to float 
                 var targetY = detection[SRLegacy.DIR.DOWN].getPoint().y + springNeutralPoint; 
                 
+                // Subtract where the missle is from the missle isn't
                 var diff    = targetY - transform.position.y;
                 
-                if (diff < -floatSpringLength || 
-                    diff > floatSpringLength)
+                // If the spring is stretched too long, lose contact
+                if (diff < -floatSpringLength)
                     diff = 0; 
             
-
+                // Only use the spring when walking
                 if (state.mode == MovementMode.WALK){ 
                     
+                    // If we are close to being neutral, snap to the target height
                     if (diff > -0.2f && diff < 0.5f && Mathf.Abs(rb.velocity.y) < 1.0f) {
-                        transform.position.Set(transform.position.x, targetY, transform.position.z);  
-                        return;
+                        rb.MovePosition(Vector3.up * diff);  
+                        return; // No spring action needed 
                     }
-
+                
+                    // Calculate spring-damper system 
                     var dampingForce = rb.velocity.y * -floatDamping;
                     var springForce = floatSpring * diff; 
 
+                    // Add the sum of the two forces 
+                    // TODO: Is the time.deltaTime really necessary? 
+                    //     * Both the damping and spring values are pretty large 
                     rb.AddForce(Vector3.up * (springForce + dampingForce) * Time.deltaTime);
 
                 }
             
             }
 #if false
+            // Debug logging of the floatation action 
+            // TIP: Use te preprocessor directive above to toggle 
                 Debug.Log(string.Format("diff: {0}, speed: {3}, spring: {1}, damping: {2}",
                             diff,
                             springForce, 
@@ -165,13 +190,16 @@ namespace Movement {
              
         }
 
+        // Find the desired speed of the player and the acceleration
         void determineAccelerationAndDesiredSpeed(out float acc, out float desiredSpeed){
                 switch (state.mode.current) {
                 case MovementMode.WALK:
+                    // Basic movement, faster when sprinting
                     acc = state.isSprinting ? o_walk.runAcc : o_walk.walkAcc;
                     desiredSpeed = state.isSprinting ? o_walk.runSpeedMax : o_walk.walkSpeedMax; 
                     break;
                 case MovementMode.SLIDE: 
+                        // TODO: Refactor this mess 
                         if(state.isGrounded) {
                             acc = 2 * o_walk.runAcc;
                              // Make sure the player is not stuck in ceiling
@@ -181,20 +209,22 @@ namespace Movement {
                             }
                             else 
                             { 
-                                
                                 // desiredSpeed = new Vector2(rb.velocity.x, rb.velocity.z).magnitude + Mathf.Clamp(-rb.velocity.y,0, float.MaxValue);
                                 desiredSpeed = new Vector3(rb.velocity.x, 
                                                             Mathf.Clamp(rb.velocity.y, float.MinValue, 0),
                                                             rb.velocity.z).magnitude;
-
+                                // Add drag when moving fast 
                                 if (desiredSpeed > o_walk.walkSpeedMax){
                                     desiredSpeed -= (o_walk.slideDrag * rb.velocity.magnitude * Time.deltaTime); // Incur drag
+                                
+                                // Stop if slow 
                                 } else if (desiredSpeed < o_walk.slideMinVel+2) {
                                     desiredSpeed = 0; 
                                 }
 
                                     
 #if true
+                                // Logging for sliding 
                                 Debug.Log(string.Format("\ncurrent: {0} | desired: {1} | can slide {2} | vspeed {3}",
                                            rb.velocity.magnitude,
                                            desiredSpeed,
@@ -205,19 +235,20 @@ namespace Movement {
                         }
                         else 
                         {
+                            // No unique behaviour when in air
                             goto case MovementMode.AIR;
                         }
                 break;
 
                 case MovementMode.JUMP:
                 case MovementMode.AIR: 
-                    // Jumping movement
-
+                    // Accelerate in air  
                     acc = o_air.inAirAcceleration;
                     desiredSpeed = Mathf.Max(o_air.inAirMaxAddedSpeed, 
                                              new Vector3(rb.velocity.x,0,rb.velocity.z).magnitude);                                                                                                                                                             
                 break;
-                default:  
+                default:
+                        // Edge case: Disable movement 
                         acc = 0; 
                         desiredSpeed = 0; 
                 break;
@@ -226,24 +257,29 @@ namespace Movement {
 
         }
 
+        // Apply acceleration to move to the desired speed in the desired direction 
         void applyLateralMovementForce(float acc, float desiredSpeed){
-                var x_movement = state.mode == MovementMode.SLIDE ? 0 : in_walk.x; 
-                var z_movement = 0.0f;
-                var _desiredSpeed = desiredSpeed; 
-
+                // Don't allow strafing while sliding 
+                float x_movement = state.mode == MovementMode.SLIDE ? 0 : in_walk.x; 
+                // Only allow forward and backward movement when on the ground 
+                //   unless allow forward is enabled
+                float z_movement = 0.0f;
                 if (state.isGrounded || o_air.allowForward) {
+                    // Slide locks you into going forward
                     z_movement = state.mode == MovementMode.SLIDE ? 1 : in_walk.y;
                 } 
+                
                 // Calculate velocity vector with acquired speed 
                 var desiredVelocity = new Vector3(x_movement,0,z_movement)
                       * desiredSpeed;
 
+                // Project the velocity on the surface we are walking on 
                 if (detection[SRLegacy.DIR.DOWN]){
-                    // Project on any plane we could be 
                     var normal = detection[SRLegacy.DIR.DOWN].getNormal();
                     desiredVelocity = Vector3.ProjectOnPlane(desiredVelocity, normal);
                 }
                 
+                // 
                 desiredVelocity += Vector3.up * rb.velocity.y;
 
                 
@@ -279,7 +315,9 @@ namespace Movement {
             }
 
         }
-
+// 
+// Messages from PlayerInput 
+// 
         void OnMove(InputValue val){
             in_walk = val.Get<Vector2>();
         }
@@ -345,6 +383,11 @@ namespace Movement {
 
         
     }
+
+//
+// Option objects 
+//
+// TODO: Move to separate file
 
     [System.Serializable]
     public class MovementState {
